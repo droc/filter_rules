e25407de47c1f1c32215d8cf3631e90fe9dc894d
droc <jigermano@gmail.com>
2012-06-15 18:24:14 -0300

    First commit

---
+++ filter_rules/__init__.py
@@ -0,0 +1,116 @@
+class CompositionalRule(object):
+    def __and__(self, other):
+        return And(self, other)
+
+    def __invert__(self):
+        return Not(self)
+
+    def __or__(self, other):
+        return Or(self, other)
+
+    def accepts(self, anObject):
+        raise NotImplementedError
+
+class CollectionAttributeIncludesFilter(CompositionalRule):
+    def __init__(self, attribute, quantifier, filter):
+        self.filter = filter
+        self.attribute = attribute
+        self.quantifier = quantifier
+
+    def accepts(self, anObject):
+        return self.quantifier(self.filter.accepts(x) for x in getattr(anObject, self.attribute))
+
+
+class And(CompositionalRule):
+    def __init__(self, l, r):
+        self.r = r
+        self.l = l
+
+    def accepts(self, anObject):
+        return self.l.accepts(anObject) and self.r.accepts(anObject)
+
+
+class Or(CompositionalRule):
+    def __init__(self, l, r):
+        self.r = r
+        self.l = l
+
+    def accepts(self, anObject):
+        return self.l.accepts(anObject) or self.r.accepts(anObject)
+
+
+class AttributeEqualsFilter(CompositionalRule):
+    def __init__(self, attribute, value):
+        self.value = value
+        self.attribute = attribute
+
+    def accepts(self, anObject):
+        return self.value == getattr(anObject, self.attribute)
+
+
+class AttributeLambdaFilter(CompositionalRule):
+    def __init__(self, attribute, l):
+        self.l = l
+        self.attribute = attribute
+
+    def accepts(self, anObject):
+        return self.l(getattr(anObject, self.attribute))
+
+
+class ConditionOnCollectionAttribute(CompositionalRule):
+    def __init__(self, collection_attribute):
+        self.collection_attribute = collection_attribute
+
+    def includes(self, quantifier, criteria):
+        return CollectionAttributeIncludesFilter(self.collection_attribute, quantifier, criteria)
+
+
+class ConditionOnAttribute(object):
+    def __init__(self, attr_name):
+        self.attr_name = attr_name
+
+    def __eq__(self, other):
+        return AttributeLambdaFilter(self.attr_name, lambda x: x == other)
+
+    def includes(self, param):
+        return AttributeLambdaFilter(self.attr_name, lambda x: param in x)
+
+
+class ForAll(CompositionalRule):
+    def __init__(self, attribute, condition):
+        self.attribute = attribute
+        self.condition = condition
+
+    def accepts(self, anObject):
+        return all([self.condition.accepts(x) for x in getattr(anObject, self.attribute)])
+
+
+class Not(CompositionalRule):
+    def __init__(self, inner):
+        self.inner = inner
+
+    def accepts(self, anObject):
+        return not self.inner.accepts(anObject)
+
+
+class Exists(CompositionalRule):
+    def __init__(self, attribute, condition):
+        self.attribute = attribute
+        self.condition = condition
+
+    def accepts(self, anObject):
+        return any(self.condition.accepts(x) for x in getattr(anObject, self.attribute))
+
+
+class IF(object):
+    def __init__(self, precedent_condition, consequent_condition):
+        self.consequent_condition = consequent_condition
+        self.precedent_condition = precedent_condition
+
+    def accepts(self, anObject):
+        return Not(self.precedent_condition).accepts(anObject) or self.consequent_condition.accepts(anObject)
+
+
+class AcceptAllFilter(CompositionalRule):
+    def accepts(self, anObject):
+        return True
---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='filter_rules',
+    version='0.2',
+    packages=['filter_rules'],
+    url='',
+    license='',
+    author='droc',
+    author_email='',
+    description=''
+)
